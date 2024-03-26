176663604ee74dbfe67080406d5e20c87f2256d2
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-03-27 02:52:43 +0900

    feat: 개인정보 보안 강화 구현

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -1,6 +1,7 @@
 package jwp.controller;
 
 import core.db.MemoryUserRepository;
+import jwp.util.UserSessionUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,14 +9,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("users", MemoryUserRepository.getInstance().findAll());
-        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-        rd.forward(req,resp);
+        if(UserSessionUtils.isLogined(req.getSession())){
+            req.setAttribute("users", MemoryUserRepository.getInstance().findAll());
+            RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+            rd.forward(req,resp);
+            return;
+        }
+        resp.sendRedirect("/user/login.jsp");
+
     }
 }
