275e31af202cd271b1fe6037e99b1eb4f4051b36
kamothi <rhkddlf7911@naver.com>
2024-03-31 15:48:55 +0900

    Feat: List update 기능

---
+++ src/main/java/jwp/controller/UpdateListController.java
@@ -0,0 +1,26 @@
+package jwp.controller;
+
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/updateForm")
+public class UpdateListController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        User user = MemoryUserRepository.getInstance().findUserById(userId);
+        req.setAttribute("users",user);
+        RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
+        rd.forward(req, resp);
+        return;
+    }
+}
