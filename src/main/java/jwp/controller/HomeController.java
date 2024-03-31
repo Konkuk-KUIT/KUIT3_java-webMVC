b6a78bb742de83949d5752f8780fee93740c45e6
김민석 <skyfox001015@naver.com>
2024-03-31 20:38:24 +0900

    feat : home.jsp로 이름 변경

--- src/main/java/jwp/controller/HomeController.java
+++ src/main/java/jwp/controller/HomeController.java
@@ -1,7 +1,5 @@
 package jwp.controller;
 
-import core.db.MemoryUserRepository;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
