8b4151bd602c3ad220f12c27b71a1df5bed98e58
김민석 <skyfox001015@naver.com>
2024-03-31 21:46:01 +0900

    feat : 요구사항 1완료

--- src/main/java/jwp/controller/HomeController.java
+++ src/main/java/jwp/controller/HomeController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/user/userList")
+@WebServlet("/")
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
