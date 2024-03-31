a5eca652b2d5fb47b01812e1a0fd3bc6ca5eeb94
park99999 <sungung1030@gmail.com>
2024-04-01 00:44:11 +0900

    feat:2-1진행줓ㅇ

---
+++ src/main/java/DispatcherServlet.java
@@ -0,0 +1,7 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
+public class DispatcherServlet extends HttpServlet {
+
+}
