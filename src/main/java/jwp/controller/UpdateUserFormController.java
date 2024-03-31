2317090591966073e3558ab11f3fd48df01c352c
park99999 <sungung1030@gmail.com>
2024-03-31 12:31:27 +0900

    feat : 1-5

--- src/main/java/jwp/controller/UpdateUserFormController.java
+++ src/main/java/jwp/controller/UpdateUserFormController.java
@@ -9,14 +9,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
 public class UpdateUserFormController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        User Loginuser = (User) value;
         User user = MemoryUserRepository.getInstance().findUserById(req.getParameter("userId"));
-        if (user != null) {
+        if (user != null && user.getUserId().equals(Loginuser.getUserId())) {
             req.setAttribute("user", user);
             RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
             rd.forward(req, resp);
