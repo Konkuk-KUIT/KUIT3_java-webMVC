9602b9c4ae61c54d1102c47615f9a114bf95e2b6
david-parkk <ji0513ji@naver.com>
2024-04-01 21:19:29 +0900

    feat: 회원 목록 본인 정보만 바꾸도록 로직 추가

--- src/main/java/jwp/controller/UpdateUserFormController.java
+++ src/main/java/jwp/controller/UpdateUserFormController.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
@@ -18,12 +19,27 @@ public class UpdateUserFormController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id=req.getParameter("userId");
-        System.out.println("id = " + id);
-        System.out.println("MemoryUserRepository.getInstance().findUserById(id).toString() = " + MemoryUserRepository.getInstance().findUserById(id).toString());
-        req.setAttribute("user",MemoryUserRepository.getInstance().findUserById(id));
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        if (value != null) {
+            User sessionUser = (User) value;
+            User user=MemoryUserRepository.getInstance().findUserById(id);
+            if(!user.isSameUser(sessionUser)){
+                RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+                rd.forward(req,resp);
+            }
+            else{
+                req.setAttribute("user",user);
+                RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
+                rd.forward(req,resp);
+            }
 
-        RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
+        }
+        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
         rd.forward(req,resp);
+
+
+
     }
 
 }
