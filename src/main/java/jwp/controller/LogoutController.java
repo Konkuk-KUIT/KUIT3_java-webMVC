87f3c37e4090db0b2d3baf243de086c9efeca960
김민석 <skyfox001015@naver.com>
2024-04-02 17:57:43 +0900

    feat : 요구사항4번 완료

---
+++ src/main/java/jwp/controller/LogoutController.java
@@ -0,0 +1,24 @@
+package jwp.controller;
+
+import core.db.MemoryUserRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/logout")
+public class LogoutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("user");
+        resp.sendRedirect("/");
+    }
+
+    }
+
