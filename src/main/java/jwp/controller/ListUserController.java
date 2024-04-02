6e7aa2e68098eb17958ab35384db18994f9417d1
junyoungBae1 <bjy556@gmail.com>
2024-04-03 00:07:47 +0900

    feat: 회원 목록 및 개인정보 수정 보안

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -1,6 +1,7 @@
 package jwp.controller;
 
 import core.db.MemoryUserRepository;
+import jwp.util.UserSessionUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,14 +9,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("users", MemoryUserRepository.getInstance().findAll());
-        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-        rd.forward(req,resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(UserSessionUtils.isLogined(request.getSession())){
+            request.setAttribute("users", MemoryUserRepository.getInstance().findAll());
+            RequestDispatcher rd = request.getRequestDispatcher("/user/list.jsp");
+            rd.forward(request,response);
+            return;
+        }
+        response.sendRedirect("/user/login.jsp");
+
     }
 }
