8b360104d453c408d22bac984a6c187e0f9ed163
JangIkhwan <87964632+JangIkhwan@users.noreply.github.com>
2024-04-03 22:00:54 +0900

    Feat : 사용자 리스트 보안 기능 추가

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -5,17 +5,20 @@ import core.db.MemoryUserRepository;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("users", MemoryUserRepository.getInstance().findAll());
-        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-        rd.forward(req,resp);
+        HttpSession session = req.getSession();
+        if(session.getAttribute("user") != null){
+            req.setAttribute("users", MemoryUserRepository.getInstance().findAll());
+            RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+            rd.forward(req,resp);
+            return;
+        }
+        resp.sendRedirect("/");
     }
 }
--- src/main/java/jwp/controller/UserUpdateFormController.java
+++ src/main/java/jwp/controller/UserUpdateFormController.java
@@ -9,17 +9,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
 public class UserUpdateFormController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        MemoryUserRepository repository = MemoryUserRepository.getInstance();
-        User user = repository.findUserById(req.getParameter("userId"));
-
-        req.setAttribute("user", user);
-        RequestDispatcher rq = req.getRequestDispatcher("/user/updateForm.jsp");
-        rq.forward(req, resp);
+        HttpSession session = req.getSession();
+        User loginedUser = (User) session.getAttribute("user");
+        String userId = req.getParameter("userId");
+        if(loginedUser.isSameUser(userId)){
+            MemoryUserRepository repository = MemoryUserRepository.getInstance();
+            User user = repository.findUserById(userId);
+            req.setAttribute("user", user);
+            RequestDispatcher rq = req.getRequestDispatcher("/user/updateForm.jsp");
+            rq.forward(req, resp);
+        }
+        resp.sendRedirect("/user/userList");
     }
 }
