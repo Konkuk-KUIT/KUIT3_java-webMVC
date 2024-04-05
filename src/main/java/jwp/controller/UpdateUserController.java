a47d9cd5290888ba3524bb1b504dfdc906973235
arkchive <kyungminkim7@konkuk.ac.kr>
2024-04-05 14:07:43 +0900

    [feat] 요구사항 3: 개인정보 수정 실습

---
+++ src/main/java/jwp/controller/UpdateUserController.java
@@ -0,0 +1,27 @@
+package jwp.controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/update")
+public class UpdateUserController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User(req.getParameter("userId"),
+                req.getParameter("password"),
+                req.getParameter("name"),
+                req.getParameter("email"));
+
+        MemoryUserRepository.getInstance().changeUserInfo(user);
+
+        resp.sendRedirect("/user/userList");
+    }
+
+}
