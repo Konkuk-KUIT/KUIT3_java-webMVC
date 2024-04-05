7c4e380501466a6e779fdc9b944c7f7c46b64663
emes-g <saltishDev@gmail.com>
2024-04-05 13:01:56 +0900

    Feat: 로그인/로그아웃 구현 (1-4)

---
+++ src/main/java/jwp/controller/LoginController.java
@@ -0,0 +1,29 @@
+package jwp.controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = MemoryUserRepository.getInstance().findUserById(req.getParameter("userId"));
+
+        if (user != null && user.getPassword().equals(req.getParameter("password"))) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            resp.sendRedirect("/");
+            return;
+        }
+        resp.sendRedirect("/user/login_failed.jsp");
+    }
+}
