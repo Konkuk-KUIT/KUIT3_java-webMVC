d08eedcbd3893e20d6d1252c2b38e8ce084c5ae5
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-04-05 22:53:27 +0900

    요구사항 5: 회원 목록 및 개인정보 수정 보안 강화

---
+++ src/main/java/jwp/util/UserSessionUtils.java
@@ -0,0 +1,18 @@
+package jwp.util;
+
+import jwp.model.User;
+
+import javax.servlet.http.HttpSession;
+
+public class UserSessionUtils {
+    public static final String USER_SESSION_KEY = "user";
+    public static User getUserFromSession(HttpSession session) {
+        Object user = session.getAttribute(USER_SESSION_KEY);
+        if (user == null) return null;
+        return (User) user;
+    }
+
+    public static boolean isLogined(HttpSession session) {
+        return getUserFromSession(session) != null;
+    }
+}
