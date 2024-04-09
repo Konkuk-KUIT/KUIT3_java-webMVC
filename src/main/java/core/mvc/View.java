0c6af5d24f308c52ae6fff583a1103903895b6f0
david-parkk <ji0513ji@naver.com>
2024-04-09 22:25:08 +0900

    refactor: View class 추가

---
+++ src/main/java/core/mvc/View.java
@@ -0,0 +1,25 @@
+package core.mvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class View {
+
+    private String viewName;
+
+    private static final String REDIRECT_PREFIX="redirect:";
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            resp.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        RequestDispatcher rd = req.getRequestDispatcher(viewName);
+        rd.forward(req, resp);
+    }
+}
