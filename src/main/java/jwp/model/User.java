cc62b9f8a21250e71defaec20fd2c147384e6e8f
hyunn522 <tjgus52213@gmail.com>
2024-04-10 18:22:09 +0900

    resolve conflict

--- src/main/java/jwp/model/User.java
+++ src/main/java/jwp/model/User.java
@@ -51,10 +51,6 @@ public class User {
         return userId.equals(this.userId) && matchPassword(password);
     }
 
-    public boolean isSameUser(String userId, String password) {
-        return userId.equals(this.userId) && password.equals(this.password);
-    }
-
     @Override
     public int hashCode() {
         final int prime = 31;
