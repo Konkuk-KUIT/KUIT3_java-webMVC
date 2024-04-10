9aca0cdee5918dc629e868ae5905be7df474cf71
kamothi <rhkddlf7911@naver.com>
2024-04-10 20:37:11 +0900

    Refact: 사용하지 않는 함수 삭제

--- src/main/java/core/mvc/view/JsonView.java
+++ src/main/java/core/mvc/view/JsonView.java
@@ -17,14 +17,4 @@ public class JsonView implements View{
         PrintWriter out = response.getWriter();
         out.print(mapper.writeValueAsString(model));
     }
-
-    private Map<String, Object> createModel(HttpServletRequest request) {
-        Enumeration<String> names = request.getAttributeNames();;
-        Map<String, Object> model = new HashMap<>();
-        while(names.hasMoreElements()) {
-            String name = names.nextElement();
-            model.put(name,request.getAttribute(name));
-        }
-        return model;
-    }
 }
