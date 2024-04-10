e4625f626a3304c7ca8ebfa8b4cd692304fe3e19
YouHyuk <cxz6411@gmail.com>
2024-04-10 17:11:50 +0900

    Feat: View 인터페이스 및 Jsp, Json 각각의 View 클래스 구현

---
+++ src/main/java/core/view/View.java
@@ -0,0 +1,8 @@
+package core.view;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface View {
+    void render(HttpServletRequest req, HttpServletResponse resp) throws  Exception;
+}
