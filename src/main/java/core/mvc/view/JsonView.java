d8b8f67ad33bea312a37ba943ee150a964456e57
Hyeon <csh-scl>
2024-04-12 01:45:46 +0900

    View render 인자 변경에 따른 JsonView, JspView render 함수 변경

--- src/main/java/core/mvc/view/JsonView.java
+++ src/main/java/core/mvc/view/JsonView.java
@@ -12,23 +12,22 @@ import java.util.Map;
 public class JsonView implements View {
 
     @Override
-    public void render(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         resp.setContentType("application/json;charset=UTF-8");
         // 응답해 주는 부분 추가
         PrintWriter out = resp.getWriter();
-        out.print(mapper.writeValueAsString(createModel(req)));
+        out.print(mapper.writeValueAsString(model));
     }
-
-    private Map<String, Object> createModel(HttpServletRequest request) {
-        // 정보를 읽어와서 넘겨주는 역할
-        Enumeration<String> names = request.getAttributeNames();
-        Map<String, Object> model = new HashMap<>();
-
-        while(names.hasMoreElements()){
-            String name = names.nextElement();
-            model.put(name, request.getAttribute(name));
-        }
-        return model;
-    }
+//    private Map<String, Object> createModel(HttpServletRequest request) {
+//        // 정보를 읽어와서 넘겨주는 역할
+//        Enumeration<String> names = request.getAttributeNames();
+//        Map<String, Object> model = new HashMap<>();
+//
+//        while(names.hasMoreElements()){
+//            String name = names.nextElement();
+//            model.put(name, request.getAttribute(name));
+//        }
+//        return model;
+//    }
 }
--- src/main/java/core/mvc/view/JspView.java
+++ src/main/java/core/mvc/view/JspView.java
@@ -4,6 +4,8 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.Map;
+
 import static core.mvc.Controller.REDIRECT;
 
 public class JspView implements View{
@@ -17,7 +19,12 @@ public class JspView implements View{
     }
 
     @Override
-    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // model에 있는 정보 넘겨주기
+        for(String key : model.keySet()) {
+            request.setAttribute(key, model.get(key));
+        }
+
         if (viewName.startsWith(REDIRECT)) {
             response.sendRedirect(viewName.substring(REDIRECT.length()));
             return;
