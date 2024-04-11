56cc1110eef9eab64c9d4703b400fad1c510ad42
JangIkhwan <87964632+JangIkhwan@users.noreply.github.com>
2024-04-11 12:14:10 +0900

    Fix : View 인터페이스 및 상속 객체 수정

--- src/main/java/core/mvc/view/JspView.java
+++ src/main/java/core/mvc/view/JspView.java
@@ -11,13 +11,8 @@ public class JspView implements View{
     private final String viewName;
     private static final String REDIRECT_PREFIX = "redirect:";
 
-
-    public JspView(String viewName) {
-        this.viewName = viewName;
-    }
-
     @Override
-    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (viewName.startsWith(REDIRECT_PREFIX)) {
             response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
             return;
@@ -26,4 +21,19 @@ public class JspView implements View{
         RequestDispatcher rd = request.getRequestDispatcher(viewName);
         rd.forward(request, response);
     }
+
+    public JspView(String viewName) {
+        this.viewName = viewName;
+    }
+
+//    @Override
+//    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        if (viewName.startsWith(REDIRECT_PREFIX)) {
+//            response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
+//            return;
+//        }
+//
+//        RequestDispatcher rd = request.getRequestDispatcher(viewName);
+//        rd.forward(request, response);
+//    }
 }
