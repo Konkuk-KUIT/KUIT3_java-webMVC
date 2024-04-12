2a4e33f8e40543be82a47f11fda7bc7928f64215
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-04-12 17:26:47 +0900

    fix: Q&A 오류 수정

--- src/main/java/jwp/controller/qna/ShowQuestionController.java
+++ src/main/java/jwp/controller/qna/ShowQuestionController.java
@@ -10,6 +10,7 @@ import jwp.model.Question;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import core.mvc.view.View;
 import core.mvc.view.JspView;
@@ -19,16 +20,22 @@ public class ShowQuestionController extends AbstractController {
     private static final MemoryAnswerRepository memoryAnswerRepository = MemoryAnswerRepository.getInstance();
 
     @Override
-    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) {
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         Long questionId = Long.parseLong(req.getParameter("questionId"));
         Question question = questionRepository.findQuestionById(questionId);
         List<Answer> answers = memoryAnswerRepository.findAnswersByQuestionId(questionId);
-
-        req.setAttribute("question", question);
-        req.setAttribute("answers", answers);
+//
+//        req.setAttribute("question", question);
+//        req.setAttribute("answers", answers);
 //        return "/qna/show.jsp";
 //        return new JspView("/qna/show.jsp");
-        return jspView(REDIRECT + "/");
+        ModelAndView mav = jspView("/qna/show.jsp");
+
+        mav.addModel("question", question);
+        mav.addModel("answers", answers);
+        return mav;
+
+
     }
 }
