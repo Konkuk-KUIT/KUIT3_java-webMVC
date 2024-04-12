1fc3b30af0057ab811be0d003a375f8789a23061
JangIkhwan <87964632+JangIkhwan@users.noreply.github.com>
2024-04-12 20:30:00 +0900

    Feat : 중복 코드 제거 위해서 AbstractController 구현

---
+++ src/main/java/jwp/controller/AbstractController.java
@@ -0,0 +1,15 @@
+package jwp.controller;
+
+import core.mvc.Controller;
+import core.mvc.view.JsonView;
+import core.mvc.view.JspView;
+import core.mvc.view.ModelAndView;
+
+public abstract class AbstractController implements Controller {
+    public ModelAndView jspView(String url){
+        return new ModelAndView(new JspView(url));
+    }
+    public ModelAndView jsonView(){
+        return new ModelAndView(new JsonView());
+    }
+}
